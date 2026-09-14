88988aa5ee17869c61ea8320f19d251676cc14da
Kristyn Le <kristynle0608@gmail.com>
2026-09-14 17:04:31 -0300

    Add greek alphabet lower and upper cases

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,8 @@
 import CardStyles from './Card.module.css'
 
 function Card() {
+    // This function should verify whether the user has clicked the right card
+    // That means that Card should receive the answer key along with the card's key
     const clickCard = () => {
         console.log("Click card")
     }
---
+++ src/constants.ts
@@ -0,0 +1,201 @@
+export const uppercaseGreek = [
+    {
+        greek: "A",
+        english: "Alpha"
+    },
+    {
+        greek: "B",
+        english: "Beta"
+    },
+    {
+        greek: "Γ",
+        english: "Gamma"
+    },
+    {
+        greek: "Δ",
+        english: "Delta"
+    },
+    {
+        greek: "E",
+        english: "Epsilon"
+    },
+    {
+        greek: "Z",
+        english: "Zeta"
+    },
+    {
+        greek: "H",
+        english: "Eta"
+    },
+    {
+        greek: "Θ",
+        english: "Theta"
+    },
+    {
+        greek: "Ι",
+        english: "Iota"
+    },
+    {
+        greek: "K",
+        english: "Kappa"
+    },
+    {
+        greek: "Λ",
+        english: "Lambda"
+    },
+    {
+        greek: "M",
+        english: "Mu"
+    },
+    {
+        greek: "N",
+        english: "Nu"
+    },
+    {
+        greek: "Ξ",
+        english: "Xi"
+    },
+    {
+        greek: "O",
+        english: "Omicron"
+    },
+    {
+        greek: "Π",
+        english: "Pi"
+    },
+    {
+        greek: "P",
+        english: "Rho"
+    },
+    {
+        greek: "Σ",
+        english: "Sigma"
+    },
+    {
+        greek: "T",
+        english: "Tau"
+    },
+    {
+        greek: "Y",
+        english: "Upsilon"
+    },
+    {
+        greek: "Φ",
+        english: "Phi"
+    },
+    {
+        greek: "Χ",
+        english: "Chi"
+    },
+    {
+        greek: "Ψ",
+        english: "Psi"
+    },
+    {
+        greek: "Ω",
+        english: "Omega"
+    }
+];
+
+export const lowercaseGreek = [
+    {
+        greek: "α",
+        english: "Alpha"
+    },
+    {
+        greek: "β",
+        english: "Beta"
+    },
+    {
+        greek: "γ",
+        english: "Gamma"
+    },
+    {
+        greek: "δ",
+        english: "Delta"
+    },
+    {
+        greek: "ε",
+        english: "Epsilon"
+    },
+    {
+        greek: "ζ",
+        english: "Zeta"
+    },
+    {
+        greek: "η",
+        english: "Eta"
+    },
+    {
+        greek: "θ",
+        english: "Theta"
+    },
+    {
+        greek: "ι",
+        english: "Iota"
+    },
+    {
+        greek: "κ",
+        english: "Kappa"
+    },
+    {
+        greek: "λ",
+        english: "Lambda"
+    },
+    {
+        greek: "μ",
+        english: "Mu"
+    },
+    {
+        greek: "ν",
+        english: "Nu"
+    },
+    {
+        greek: "ξ",
+        english: "Xi"
+    },
+    {
+        greek: "ο",
+        english: "Omicron"
+    },
+    {
+        greek: "π",
+        english: "Pi"
+    },
+    {
+        greek: "ρ",
+        english: "Rho"
+    },
+    {
+        greek: "σ",
+        english: "Sigma"
+    },
+    {
+        greek: "ς",
+        english: "Sigma"
+    },
+    {
+        greek: "τ",
+        english: "Tau"
+    },
+    {
+        greek: "υ",
+        english: "Upsilon"
+    },
+    {
+        greek: "φ",
+        english: "Phi"
+    },
+    {
+        greek: "χ",
+        english: "Chi"
+    },
+    {
+        greek: "ψ",
+        english: "Psi"
+    },
+    {
+        greek: "ω",
+        english: "Omega"
+    }
+]
